bad82582a158b5e88b18307e87c30d83158d4550
dan-1-dev <146943710+dan-1-dev@users.noreply.github.com>
2026-02-13 09:18:46 -0300

    Update view.js
    1111

--- js/view.js
+++ js/view.js
@@ -1,6 +1,6 @@
 export const view = {
   update({ days1, days, hours, minutes, seconds }) {
-    document.getElementById("dias1").textContent = days1;
+    // document.getElementById("dias1").textContent = days1;
     document.getElementById("dias").textContent = days;
     document.getElementById("horas").textContent = hours;
     document.getElementById("minutos").textContent = minutes;
