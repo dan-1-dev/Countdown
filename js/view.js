aedde1b8818dbdceaca3dcf7e397ff87e6f011c6
dan-1-dev <146943710+dan-1-dev@users.noreply.github.com>
2026-02-13 09:24:21 -0300

    Update view.js
    desfeito

--- js/view.js
+++ js/view.js
@@ -1,5 +1,5 @@
 export const view = {
-  update({ days1, days, hours, minutes, seconds }) {
+  update({ days, hours, minutes, seconds }) {
     // document.getElementById("dias1").textContent = days1;
     document.getElementById("dias").textContent = days;
     document.getElementById("horas").textContent = hours;
