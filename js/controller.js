4ef0ba587288d7da32b29b298acfb1fa4fff935e
dan-1-dev <146943710+dan-1-dev@users.noreply.github.com>
2026-02-13 08:57:24 -0300

    Update controller.js
    add interval 01/05/2026

--- js/controller.js
+++ js/controller.js
@@ -2,15 +2,23 @@ import { model } from "./model.js";
 import { view } from "./view.js";
 
 function updateCountdown() {
+  const time1 = model.getTimeRemaining1();
   const time = model.getTimeRemaining();
 
+  if (time1.total <= 0) {
+    clearInterval(interval);
+    return;
+  }
   if (time.total <= 0) {
     clearInterval(interval);
     return;
   }
 
+  view.update(time1);
   view.update(time);
+  
 }
-
+updateCountdown1(); // Atualiza ao carregar
 updateCountdown(); // Atualiza ao carregar
+const interval1 = setInterval(updateCountdown1, 1000); // Atualiza a cada segundo
 const interval = setInterval(updateCountdown, 1000); // Atualiza a cada segundo
