f41d681c4094600ddf783d2f57e1d81b153b9232
dan-1-dev <146943710+dan-1-dev@users.noreply.github.com>
2026-02-13 09:23:50 -0300

    Update model.js
    desfeito

--- js/model.js
+++ js/model.js
@@ -1,27 +1,27 @@
 export const model = {
-  targetDate1: new Date("2026-05-01T00:00:00"),
+  // targetDate1: new Date("2026-05-01T00:00:00"),
   targetDate: new Date("2026-05-29T00:00:00"),
 
-  getTimeRemaining1() {
-    const now = new Date();
-    const total = this.targetDate1 - now;
+  // getTimeRemaining1() {
+  //   const now = new Date();
+  //   const total = this.targetDate1 - now;
 
-    const days1 = Math.floor(total / (1000 * 60 * 60 * 24));
+  //   const days1 = Math.floor(total / (1000 * 60 * 60 * 24));
    
 
-    let daysLeft = Math.ceil(total / days1);
+  //   let daysLeft = Math.ceil(total / days1);
 
-    // Limitar entre 0 e 365
-    daysLeft = Math.max(0, Math.min(365, daysLeft));
-    // Cálculo de posição (trajeto de 365px, 1px por dia)
-    const totalWidth = 365;
-    const currentPosition = totalWidth - daysLeft;
-    const walker = document.getElementById("walker");
-    //walker.style.left = `${currentPosition}px`;
-    //document.getElementById("daysLeftDisplay");
+  //   // Limitar entre 0 e 365
+  //   daysLeft = Math.max(0, Math.min(365, daysLeft));
+  //   // Cálculo de posição (trajeto de 365px, 1px por dia)
+  //   const totalWidth = 365;
+  //   const currentPosition = totalWidth - daysLeft;
+  //   const walker = document.getElementById("walker");
+  //   //walker.style.left = `${currentPosition}px`;
+  //   //document.getElementById("daysLeftDisplay");
 
-    return { total, days1, };
-  }
+  //   return { total, days1, };
+  // }
     getTimeRemaining() {
     const now = new Date();
     const total = this.targetDate - now;
