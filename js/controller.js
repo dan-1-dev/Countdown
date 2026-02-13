9c6e4c0ac3677feb528ab13d81928d157c51bf67
dan-1-dev <146943710+dan-1-dev@users.noreply.github.com>
2026-02-13 09:07:13 -0300

    Update controller.js
    atualizando count1

--- js/controller.js
+++ js/controller.js
@@ -1,24 +1,29 @@
 import { model } from "./model.js";
 import { view } from "./view.js";
 
-function updateCountdown() {
+function updateCountdown1() {
   const time1 = model.getTimeRemaining1();
-  const time = model.getTimeRemaining();
 
   if (time1.total <= 0) {
     clearInterval(interval);
     return;
   }
+
+  view.update(time1);
+  
+}
+function updateCountdown() {
+  const time = model.getTimeRemaining();
+
   if (time.total <= 0) {
     clearInterval(interval);
     return;
   }
-
-  view.update(time1);
+ 
   view.update(time);
   
 }
-// Atualiza ao carregar
+updateCountdown1();// Atualiza ao carregar
 updateCountdown(); // Atualiza ao carregar
 const interval1 = setInterval(updateCountdown1, 1000); // Atualiza a cada segundo
 const interval = setInterval(updateCountdown, 1000); // Atualiza a cada segundo
