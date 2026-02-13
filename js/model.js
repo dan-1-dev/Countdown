6c5121e40df3677efcd0d4d07c5f83053aa2415c
dan-1-dev <146943710+dan-1-dev@users.noreply.github.com>
2026-02-13 09:32:40 -0300

    Update model.js
    update ok

--- js/model.js
+++ js/model.js
@@ -1,6 +1,7 @@
 export const model = {
   // targetDate1: new Date("2026-05-01T00:00:00"),
   targetDate: new Date("2026-05-29T00:00:00"),
+  beginDate: new Date("2026-05-01T00:00:00"),
 
   // getTimeRemaining1() {
   //   const now = new Date();
@@ -25,11 +26,13 @@ export const model = {
     getTimeRemaining() {
     const now = new Date();
     const total = this.targetDate - now;
+    const inicio = this.beginDate - now;
 
     const seconds = Math.floor((total / 1000) % 60);
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    const begin = Math.floor(inicio / (1000 * 60 * 60 * 24));
 
     let daysLeft = Math.ceil(total / days);
 
@@ -42,6 +45,6 @@ export const model = {
     //walker.style.left = `${currentPosition}px`;
     //document.getElementById("daysLeftDisplay");
 
-    return { total, days, hours, minutes, seconds };
+    return { total, days, hours, minutes, seconds, begin };
   }
 };
